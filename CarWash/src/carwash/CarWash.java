0b77c65e03c1648b868e83fd6d9bc0f97b5c3acf
Leticia-alt <petrinaleticia@gmail.com>
2026-06-12 15:37:55 -0300

    Add files via upload

---
+++ CarWash/src/carwash/CarWash.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package carwash;
+
+/**
+ *
+ * @author Pc0w0
+ */
+public class CarWash {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
